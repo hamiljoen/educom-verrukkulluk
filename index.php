0015b320a9c37cd87dc1c12c3fadb57a47c7a158
Randy Hamilton <113764657+hamiljoen@users.noreply.github.com>
2022-11-14 15:21:33 +0100

    Opdracht #6

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 
 require_once("lib/database.php");
 require_once("lib/artikel.php");
+require_once("lib/user.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
+$usr = new user($db->getConnection());
 
 
 /// VERWERK 
-$data = $art->selecteerArtikel(3);
+$data = $usr->selecteerUser(1); 
 
 /// RETURN
 var_dump($data);
