b3e91aceeabb82efa5956b74b91e5abd2ec54174
Randy Hamilton <113764657+hamiljoen@users.noreply.github.com>
2022-11-23 12:24:06 +0100

    Add delete favorite #8

--- lib/info.php
+++ lib/info.php
@@ -17,16 +17,41 @@ class info {
 
     }
     
-    //selectie ingredient info
+    //    addFavoriet ($gerecht_id, $record_type)
+
+    public function addFavoriet($gerecht_id, $record_type) {
+
+        $sql = "INSERT INTO info (gerecht_id, record_type, user_id)\n";
+        $return = [];
+
+        $result = mysqli_query($this->connection, $sql);
+        $arr = [];
+
+    }
+
+        //    deleteFavoriet ($gerecht_id, $record_type)
+
+        public function deleteFavoriet($gerecht_id, $record_type) {
+
+            $sql = "DELETE O FROM info WHERE record_type = '$record_type'";
+    
+            $result = mysqli_query($this->connection, $sql);
+            $arr = [];
+    
+        }
+
+    //      selectie ingredient info
 
     public function selecteerInfo($gerecht_id, $record_type) {
         
         $sql = "SELECT * FROM info WHERE gerecht_id = '$gerecht_id' AND record_type = '$record_type'";
+        $return = [];
 
         $result = mysqli_query($this->connection, $sql);
         $arr = [];
 
             while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+
                 if ($record_type == "O" || $record_type == "F") { 
                     $user_id = $row ["user_id"];
                     $user = $this->usr->selecteerUser($user_id);
@@ -37,16 +62,18 @@ class info {
                         "record_type" => $row['record_type'],
                         "user_id" => $row['user_id'],
                         "datum" => $row['datum'],
+                        "user" => $user,
                         "nummeriekveld" => $row['nummeriekveld'],
                         "tekstveld" => $row['tekstveld']
 
                     ];
 
-                    // return
 
-                    return($arr);
+             return($arr);
+    }       
 
-    }
+            return($arr);
+
 
 }
     }}
